49358070f59bf4583508bc71abf6bc885b222cec
zccccc01 <zccccc01@users.noreply.github.com>
2025-03-18 09:00:40 +0000

    deploy: ZCST-OpenCS/ZCST-OpenCS-Source@04f93d3cc2c5d4487fb5862a9b663379e3a1eebf

---
+++ assets/chunks/CourseButton.BUDUF312.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as r,t as s}from"./framework.DPDPlp3K.js";const c=["href"],n={__name:"CourseButton",props:{text:String,url:String},setup(t){return(a,u)=>(r(),o("a",{href:t.url,class:"course-button"},s(t.text),9,c))}},i=e(n,[["__scopeId","data-v-67199822"]]);export{i as C};
